e453ab46b4315ab2880c2d0d13db1443dad5bc23
Marcisbee <marcisbergmanis@gmail.com>
2019-10-19 23:06:41 +0300

    Error handler fix

--- src/routes/settings/settings.tsx
+++ src/routes/settings/settings.tsx
@@ -97,14 +97,14 @@ function formatErrorMessage(error: any, setErrors: any): string | undefined {
   const firstError = error.graphQLErrors
     && error.graphQLErrors[0];
 
-  if (firstError.extensions
+  if (firstError && firstError.extensions
     && firstError.extensions.code === 'BAD_USER_INPUT'
     && firstError.extensions.validation) {
     setErrors(firstError.extensions.validation);
     return;
   }
 
-  return firstError.message || error.message;
+  return (firstError && firstError.message) || error.message;
 }
 
 const Settings: React.FC = () => {
