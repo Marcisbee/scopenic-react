18c22e993f523558e9fb2d925b46fb02acc54096
Marcisbee <marcisbergmanis@gmail.com>
2019-12-09 22:56:11 +0200

    Input knobs

--- src/routes/editor/components/editor-right/editor-right.tsx
+++ src/routes/editor/components/editor-right/editor-right.tsx
@@ -1,3 +1,4 @@
+import cc from 'classcat';
 import dlv from 'dlv';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 
@@ -6,6 +7,7 @@ import { useRefsContext } from '../../../../utils/refs-context';
 import { EditorStore } from '../../context/editor-context';
 
 interface INumberInputProps {
+  label?: string;
   min?: number;
   max?: number;
   value?: number | string;
@@ -31,6 +33,7 @@ function separateValueFromMetric(data: string | number) {
 }
 
 const NumberInput: React.FC<INumberInputProps> = ({
+  label,
   min = Number.NEGATIVE_INFINITY,
   max = Number.POSITIVE_INFINITY,
   value: defaultValue = 0,
@@ -42,12 +45,13 @@ const NumberInput: React.FC<INumberInputProps> = ({
   const value = normalised.value;
   const metric = defaultMetric || normalised.metric || 'px';
 
-  const [isInvalid, setIsInvalid] = useState(true);
+  const [isInvalid, setIsInvalid] = useState(false);
   const [isPointerLocked, setIsPointerLocked] = useState(false);
+  const handleRef = useRef<HTMLInputElement>();
   const selfRef = useRef<HTMLInputElement>();
 
   function handleMouseDown() {
-    if (!isPointerLocked && !readOnly && selfRef.current) { selfRef.current.requestPointerLock(); }
+    if (!isPointerLocked && !readOnly && handleRef.current) { handleRef.current.requestPointerLock(); }
   }
   function handleMouseUp() {
     if (isPointerLocked && !readOnly) { document.exitPointerLock(); }
@@ -59,7 +63,7 @@ const NumberInput: React.FC<INumberInputProps> = ({
   }
 
   function handlePointerLockChange() {
-    const locked = document.pointerLockElement === selfRef.current;
+    const locked = document.pointerLockElement === handleRef.current;
     setIsPointerLocked(locked);
   }
 
@@ -130,7 +134,7 @@ const NumberInput: React.FC<INumberInputProps> = ({
       selfRef.current.value = String(inputValue);
     }
 
-    if (!isInvalid && !valid) {
+    if (!valid) {
       setIsInvalid(true);
       return false;
     }
@@ -162,20 +166,47 @@ const NumberInput: React.FC<INumberInputProps> = ({
   }, [value, metric]);
 
   return (
-    <>
-      <input
-        type="text"
-        ref={selfRef as any}
-        defaultValue={`${value}${metric}`}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}
-        onChange={() => { }}
-        onInput={onInput}
-        onKeyDown={onKeyDown}
-      />
-      {isInvalid && 'Invalid value'}
-    </>
+    <label className="row">
+      <span className="col-xs-4">
+        {label && (
+          <span style={{ display: 'block', fontSize: 13, paddingTop: 6 }}>
+            {label}
+          </span>
+        )}
+      </span>
+
+      <span className="col-xs-8">
+        <span className="pt-small pt-control-group pt-fill">
+          <span className="pt-input-group">
+            <span
+              ref={handleRef as any}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+              onMouseMove={handleMouseMove}
+              className="pt-icon im im-reset"
+            />
+            <input
+              type="text"
+              ref={selfRef as any}
+              className={cc([
+                'pt-input',
+                isInvalid && 'pt-intent-danger',
+              ])}
+              defaultValue={`${value}${metric}`}
+              onChange={() => { }}
+              onInput={onInput}
+              onKeyDown={onKeyDown}
+            />
+          </span>
+        </span>
+
+        {isInvalid && (
+          <span className="pt-input-helper-text">
+            Invalid value
+          </span>
+        )}
+      </span>
+    </label>
   );
 };
 
@@ -210,6 +241,7 @@ const EditorRight: React.FC = () => {
     backgroundColor: cssDeclarations && cssDeclarations.backgroundColor,
     color: cssDeclarations && cssDeclarations.color,
     fontSize: cssDeclarations && cssDeclarations.fontSize,
+    lineHeight: cssDeclarations && cssDeclarations.lineHeight,
   };
 
   if (!currentClassName) {
@@ -223,8 +255,8 @@ const EditorRight: React.FC = () => {
       <KnobsBlock
         title="Text format"
       >
-        Font size
         <NumberInput
+          label="Font Size"
           min={1}
           value={currentStyles.fontSize || defaultStyles.fontSize}
           onChange={(value) => {
@@ -232,6 +264,19 @@ const EditorRight: React.FC = () => {
               id: element.id,
               className: currentClassName,
               property: 'fontSize',
+              value,
+            });
+          }}
+        />
+        <NumberInput
+          label="Line Height"
+          min={1}
+          value={currentStyles.lineHeight || defaultStyles.lineHeight}
+          onChange={(value) => {
+            updateStylePropery({
+              id: element.id,
+              className: currentClassName,
+              property: 'lineHeight',
               value,
             });
           }}
